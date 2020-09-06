456fad52c9bee66a3d6fdc4a5affb6d99daa6df4
Aaron France <aaron.l.france@gmail.com>
2020-09-06 23:24:25 +0200

    Initial commit

---
+++ package.lisp
@@ -0,0 +1,24 @@
+(defpackage :weird-pointers
+  (:use :cl :bordeaux-threads :cffi))
+
+(in-package :weird-pointers)
+
+(defparameter lock (bordeaux-threads:make-lock))
+(defparameter store (make-hash-table))
+
+(defun save (v)
+  (when v
+    (let ((p (cffi:foreign-alloc :int)))
+      (bordeaux-threads:with-lock-held (lock)
+        (setf (gethash p store) v)))))
+
+(defun restore (p)
+  (when p
+    (bordeaux-threads:with-lock-held (lock)
+      (gethash p store))))
+
+(defun free (p)
+  (when p
+    (bordeaux-threads:with-lock-held (lock)
+      (remhash p store)
+      (cffi:foreign-free p))))
---
+++ weird-pointers.asd
@@ -0,0 +1,8 @@
+(defsystem :weird-pointers
+  :author "Aaron France"
+  :version "0.0.1"
+  :licence "WTFPL"
+  :serial t
+  :description "Naughty naughty"
+  :components ((:file "package"))
+  :depends-on (:bordeaux-threads :cffi))
